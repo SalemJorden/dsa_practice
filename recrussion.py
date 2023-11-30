ce76eef679e38f51ca719734a98bf3b87a3d5bb0
Jared Chapman <deraj@google.com>
2023-11-30 15:07:49 -0600

    Jared added stuff

---
+++ recrussion.py
@@ -0,0 +1,24 @@
+## Write a function, sumNums, that takes a number, num, as an argument and returns 
+## the sum of all the numbers between 1 and num. You can assume that num will be 
+## greater than 1. Use recursion.
+## sumNums(3); // => 6 (3 + 2 + 1)
+
+## Take a number then write a function that subtracts until we get the base
+## case of 1. Then create another base case where if the number is 1 return number.
+
+
+# Salem & Jared
+# Jared wrote psuedo code. 
+# Salem wrote function. 
+
+def sumNums(num):
+    if num == 1:
+        print(1)
+    else:
+      num == 1:
+    print(num + sumNums(num - 1))
+
+sumNums(25);
+sumNums(18);
+sumNums(1);
+
