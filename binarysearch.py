68f87143ab92a4d982f4c3ffd383fd124369751f
Salem Jorden <115185670+SalemJorden@users.noreply.github.com>
2023-12-01 12:36:50 -0600

    Update binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,12 @@
+# Binary Search Practice
+# Leet Code Problem : 
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+# 1.) Summary of problem:
+
+# 2.) Test cases
+
+# 3.) Psuedocode solution
